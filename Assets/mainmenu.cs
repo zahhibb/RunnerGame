aceaa0964f7fef8b8b6e01d2756321db7e998c5d
Vester <vesterbeatrice@gmail.com>
2022-04-05 11:05:49 +0200

    UI canvas i Beas scene!

--- Assets/mainmenu.cs
+++ Assets/mainmenu.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class mainmenu : MonoBehaviour {
+public class mainmenu : MonoBehaviour 
+{
+    public void PlayGame () 
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-public void PlayGame ()
-{ 
-  SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-}
-
-    public void QuitGame()
+    public void QuitGame() 
     {
+        Debug.Log("quit");
         Application.Quit();
     }
 
